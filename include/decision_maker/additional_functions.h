a57ffeb3000d6e5719616bd4ac09e6eccb1db13c
yias <iasonbatz@gmail.com>
2016-08-24 17:12:12 +0200

    first commit

---
+++ include/decision_maker/additional_functions.h
@@ -0,0 +1,266 @@
+#ifndef ADDITIONAL_FUNCTIONS_H
+#define ADDITIONAL_FUNCTIONS_H
+
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <math.h>
+
+
+
+std::vector< std::vector<double> > handConfiguations(int hand, int nb_classes){
+    /*
+     * This function returns the final configurations of the joints of the Allegro hand.
+     * The number of rows of the returned matrix is nb_classes+1. The extra row is for
+     * the idle state (open palm) and it is always stored in the first row of the matrix.
+     * The second row corresponds to class 1, the third row corresponds to class 2, etc.
+     * The classes are considered in the order: precision disk, tripod, thumb-2 fingers,
+     * thumb-4 fingers, ulnar grasp.
+     *
+     * The input hand corresponds to the choice of hand:
+     *
+     *      0: left hand
+     *      1: right hand
+     *  other: it returns the configuration of the idle state (open palm)
+     *
+     *
+     */
+
+    int nbAllegroJoints=16;     // number of the joints of the Allegro hand
+
+    std::vector< std::vector<double> > configuration(nb_classes+1,std::vector<double>(nbAllegroJoints));
+
+
+
+    switch (hand+1) {
+    case 1:
+        {
+        // set the configurations for the left hand
+
+        double pos0[]={0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8294999999999999, 0.0, 0.0, 0.0};
+        configuration[0].assign(pos0,pos0+nbAllegroJoints);
+
+        double pos1[]={-0.04237095869508623, 0.5470453333881532, 0.7437613579286353, 0.8246622519939837, 0.028702179278624937, 0.21415387491454108, 1.0614804723606348, 0.37650835547609685, 0.1284833816946608, 0.41054355747717247, 1.1237207395184723, 0.42723059373656896, 1.3493727871788868, 0.28406342362896075, -0.08937179089510226, 1.257289923016124};
+
+        configuration[1].assign(pos1,pos1+nbAllegroJoints);
+
+        double pos2[]={-0.1271049481804714, 0.7094641788588949, 0.7146115325008343, 0.8238409621831889, -0.07129752052943118, 0.8265867914431462, 0.6850142232442048, 0.7839548430859884, -0.01553320216526276, -0.14385972598926425, 0.022219205813237504, -0.025653460750270383, 1.34823913866758, 0.35977134415753875, 0.12314967349942667, 1.0725716422955927};
+        configuration[2].assign(pos2,pos2+nbAllegroJoints);
+
+        double pos3[]={-0.05849754987579765, 1.0670753939401456, 0.6826479885104186, 0.8606031944835235, 0.00481340303919539, 1.0466559414495369, 0.8283367339626594, 0.8768701371658273, -0.015603029451737777, -0.14356221306852357, 0.021642462422132115, -0.02650620338071224, 1.3198479914680814, 0.2713195831034956, -0.013571358366547017, 1.2570757463955298};
+        configuration[3].assign(pos3,pos3+nbAllegroJoints);
+
+        double pos4[]={-0.08313273406077283, 1.0626900627500735, 0.6776767577766083, 0.8689894857574274, 0.01085244168119158, 1.0478904498503627, 0.8153515553104972, 0.8706244017438871, 0.08474530007832862, 0.9757269131410359, 0.8916851570471536, 0.6973578145107358, 1.3767011967950054, 0.31613809911144924, 0.15428263805282766, 0.9440189891989741};
+        configuration[4].assign(pos4,pos4+nbAllegroJoints);
+
+        double pos5[]={-0.05012838772059667, -0.13433793254175375, -0.04671840751037163, 0.04517307903339739, 0.009254868475132948, -0.2005495252070131, 0.30151401910510633, 0.028854511477014885, -0.32906246400334416, 0.9333724582174228, 1.1422871771730567, 0.212455065212159, 1.3716523471127307, 0.46959754664105635, 0.17048521323225668, 1.4742483963649067};
+        configuration[5].assign(pos5,pos5+nbAllegroJoints);
+        }
+        break;
+    case 2:
+        {
+        // set the configurations for the right hand
+
+        double pos0[]={0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.821205, 0.0, 0.0, 0.0};
+        configuration[0].assign(pos0,pos0+nbAllegroJoints);
+
+        double pos1[]={0.0, 0.6366369240000003, 0.7728741899999998, 0.7656254100000002, 0.0, 0.5352607260000002, 0.8123945939999999, 0.80434827, 0.0, 0.5865746039999999, 0.8123945939999999, 0.9586917449999999, 1.342557216, 0.03739903200000002, 0.068395536, 0.7969718789999998};
+        configuration[1].assign(pos1,pos1+nbAllegroJoints);
+
+        double pos2[]={0.0, 0.788075442, 0.7987861530000002, 0.8428884750000004, 0.0, 0.750349908, 0.7728741900000002, 0.80434827, 0.0851499, 0.019976418000000017, -0.08035641899999998, 0.058202595000000044, 1.366224453, 0.11711185200000002, 0.272908845, 0.7969718789999998};
+        configuration[2].assign(pos2,pos2+nbAllegroJoints);
+
+        double pos3[]={0.0, 1.1154472560000002, 0.6548722290000002, 0.8428884750000004, 0.0, 1.25329743, 0.35287668900000013, 0.8686428300000004, 0.0851499, 0.019976418000000017, -0.08035641899999998, 0.058202595000000044, 1.366224453, 0.16129911600000005, 0.2600246880000001, 0.7969718789999998};
+        configuration[3].assign(pos3,pos3+nbAllegroJoints);
+
+        double pos4[]={0.0, 1.1154472560000002, 0.6548722290000002, 0.8428884750000004, 5e-324, 1.2790437659999998, 0.35287668900000013, 0.9844461000000003, 5e-324, 1.1783827440000003, 0.549546624, 0.7142993549999999, 1.366224453, 0.16129911600000005, 0.2600246880000001, 0.7969718789999998};
+        configuration[4].assign(pos4,pos4+nbAllegroJoints);
+
+        double pos5[]={0.10584400000000005, 0.08374940000000003, -0.0003873999999999822, 0.0, 0.0, 0.0, -0.0003873999999999822, 0.0, 0.364062, 1.0376786000000002, 0.7675000000000001, 0.4226245000000002, 1.396, 0.4753636, 0.4822446000000001, 0.8050221};
+        configuration[5].assign(pos5,pos5+nbAllegroJoints);
+        }
+        break;
+    default:
+        double pos0[]={0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.0, 0.821205, 0.0, 0.0, 0.0};
+        configuration[0].assign(pos0,pos0+nbAllegroJoints);
+        configuration[1].assign(pos0,pos0+nbAllegroJoints);
+        configuration[2].assign(pos0,pos0+nbAllegroJoints);
+        configuration[3].assign(pos0,pos0+nbAllegroJoints);
+        configuration[4].assign(pos0,pos0+nbAllegroJoints);
+        configuration[5].assign(pos0,pos0+nbAllegroJoints);
+
+        break;
+    }
+
+
+    return configuration;
+
+}
+
+
+
+
+int majority_vote(std::vector<int> Votes, int nb_classes, double threshold, int previous_grasp){
+/*
+ * This function finds which class has the most appearances in the vector 'Votes'
+ *
+ *
+ */
+
+
+    //std::cout<<"maj vote okokkokoko\n";
+
+    int nb_votes=Votes.size();
+
+    std::vector<int> classes(nb_classes,0);
+
+    std::vector<int>::iterator result;
+    for(int j=0;j<nb_votes;j++){
+        for(int k=1;k<6;k++){
+            if (Votes[j]==k){
+                classes[k-1]++;
+            }
+        }
+    }
+
+
+    result=std::max_element(classes.begin(),classes.end());
+    std::cout<<" majority vote: "<<std::distance(classes.begin(),result)+1<<"\n";
+
+    std::cout<<" confidence "<<(double)classes[std::distance(classes.begin(),result)/Votes.size()]<<"\n";
+
+    //if((Votes.size()>2)&&(classes[std::distance(classes.begin(),result)]/Votes.size()>threshold)){
+       //std::cout<<" majority vote: "<<std::distance(classes.begin(),result)+1<<"\n";
+
+        return std::distance(classes.begin(),result)+1;
+
+    //}else{
+    //   return previous_grasp;
+    //}
+
+
+
+}
+
+
+double tm2double(int sec,int nsec){
+    /*
+     * This function returns a double combining the two input integers, making them a floating point number
+     *
+     */
+
+    std::string dd=std::to_string(sec)+"."+std::to_string(nsec);
+
+    return std::stod(dd);
+}
+
+
+std::vector<double> calcDtVelocity(std::vector<double> currentPos,std::vector<double> oldPos, double dt){
+    /*
+     * This function calculates the instantaneous velocity between two consecutive samples
+     * Inputs:
+     *
+     *      currentPos: the current position
+     *      oldPos:     the previous position
+     *      dt:         time between samples (1/samplerate)
+     *
+     * Output:
+     *
+     *      a vector with the velocities to all the directions
+     *
+     */
+
+
+    std::vector<double> velocity(currentPos.size(),0);
+
+    for(int i=0;i<(int)currentPos.size();i++){
+        velocity[i]=(currentPos[i]-oldPos[i])/dt;
+    }
+
+    return velocity;
+}
+
+
+std::vector<double> calvAverageVelocity(std::vector< std::vector<double> > mVel, int samplesBack){
+    /*
+     * This function calculates the average velocity in a timw window of 'samplesBack' samples.
+     *
+     */
+
+    std::vector<double> averVel(mVel.size(),0);
+
+    if (samplesBack<(int)mVel[0].size()){
+        for(int i=0;i<samplesBack;i++){
+            for(int j=0;j<(int)mVel.size();j++){
+                averVel[j]+=mVel[j][(int)mVel[j].size()-i];
+            }
+        }
+        for(int j=0;j<(int)mVel.size();j++){
+            averVel[j]=averVel[j]/samplesBack;
+        }
+    }else{
+        for(int i=0;i<(int)mVel[0].size();i++){
+            for(int j=0;j<(int)mVel.size();j++){
+                averVel[j]+=mVel[j][(int)mVel[j].size()-i];
+            }
+        }
+        for(int j=0;j<(int)mVel.size();j++){
+            averVel[j]=averVel[j]/((int)mVel[0].size());
+        }
+    }
+    return averVel;
+}
+
+
+double velocityNorm(std::vector< std::vector<double> > mVel, int samplesBack){
+    /*
+     * This function calculates the first norm of the average velocity in a time window of samplesBack' samples.
+     *
+     */
+
+    std::vector<double> avVel=calvAverageVelocity(mVel, samplesBack);
+
+    double norm=0;
+    for(int i=0;i<(int)avVel.size();i++){
+        norm+=avVel[i]*avVel[i];
+    }
+
+    return sqrt(norm);
+
+}
+
+
+bool check_velocity(double velocity, double threshold){
+    /*
+     * This function checks if the velocity of an object is larger than a threshold
+     *
+     */
+
+    //std::cout<<"check vel okokkokoko\n";
+
+
+    if(velocity>=threshold){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // ADDITIONAL_FUNCTIONS_H
---
+++ src/decisionMaker.cpp
@@ -0,0 +1,438 @@
+
+
+
+
+
+#include "ros/ros.h"
+
+#include <geometry_msgs/PoseStamped.h>
+#include <decision_maker/vtmsg.h>
+#include "sensor_msgs/JointState.h"
+
+
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <termios.h>
+
+
+#include "decision_maker/MatlabLib.h"
+#include "decision_maker/additional_functions.h"
+
+
+struct stat st = {0};
+
+enum hand{
+    left= 0,
+    right=1,
+};
+
+int sRate=300;                                                    // set the sample rate (Hz)
+int trialCounter=0;                                              // counter for the trials
+double startTime;
+
+
+
+/*-- Variables related to the mocap system --*/
+
+int mocapCounter=0;                                              // counter for messages from the mocap system
+double lookBack=0.1;                                             // the timewindow to look back for the average velocity
+double velThreshold=0.018;                                       // velocity threshold for destinguish the motion or no=motion of the hand
+int mocapRate=250;                                               // the sample rate of the motion capture system
+
+std::vector<double> mocapPosition(3,0);                          // vector for the position of the hand
+std::vector<double> mocapVelocity(3,0);                          // vector for the velocity of the hand
+
+std::vector< std::vector<double> > mocapHistoryPosition(3);      // vector for the position of the marker on hand
+std::vector< std::vector<double> > mocapHistoryVelocity(3);      // vector for velocity of the marker on hand
+std::vector<double> velocityNormHistory;                         // vector for the norm of the velocity of the hand
+
+std::vector<double> mocapTime;                                   // timestamp for the mocap system
+std::vector<double> checkVelocityHistory;                           // history of the velocity checking
+
+
+/*-- Variables related to the windows system --*/
+
+int daqCounter=0;                                                // counter for messages from the windows machine
+
+int nbClasses=5;                                                 // number of different classes
+
+int grasp_type=0;                                                // the outecome of the majority vote
+
+double grasp_threshold=0.2;                                      // confidence threshold to change the grasp
+
+std::vector<int> mVotes;                                         // a vector to store the classification outcome for each time window
+
+std::vector<int> graspTypeHistory;                               // a vector with all the grasptypes
+
+std::vector<double> graspTime;                                   // the time stamp of the listener of the windows machine
+
+
+
+/*-- Variables related to the allegro hand --*/
+
+int allegroCounter=0;                                            // counter for messages from the mocap system
+
+std::vector< std::vector<double> > rightHandHistory(17);         // a matrix to store the joint angles of the right Allego hand (the extra row corresponds to the time stamp)
+std::vector< std::vector<double> > leftHandHistory(17);          // a matrix to store the joint angles of the left Allego hand (the extra row corresponds to the time stamp)
+
+double initialTime;                                              // the time that the first message with the joint angles of the Allegro hand was reveived
+
+
+/*-- functions for the system --*/
+
+void saveRecordings();                                           // a function to save the data after each trial
+int getch_();                                                    // a function to catch a key press asynchronously
+
+
+
+
+/*-- Callback functions --*/
+
+void daqListener(const decision_maker::vtmsg daqmsg){
+
+
+
+    /*-- Callback function for subscriber of the windows machine --*/
+
+    graspTime.push_back((ros::Time::now().toSec())-startTime);
+
+
+
+    if(daqCounter>2){
+        graspTime.push_back((ros::Time::now().toSec())-startTime);
+    mVotes.push_back(daqmsg.vote);
+
+    //std::cout<<"daq listener okokkokoko\n";
+
+    //checkVelocityHistory.push_back(check_velocity(velocityNormHistory.back(),velThreshold));
+
+    if(check_velocity(velocityNormHistory.back(),velThreshold)) {
+
+        //grasp_type=majority_vote(mVotes,nbClasses, grasp_threshold,grasp_type);
+        grasp_type=5;
+        std::cout<<"grasp type: "<<grasp_type<<"\n";
+        graspTypeHistory.push_back(grasp_type);
+
+    }
+    }
+
+    daqCounter++;
+
+    //ROS_INFO("I heard: [%d] messages from daq\n", daqCounter);
+}
+
+void mocapListener(const geometry_msgs::PoseStamped& mocapmsg){
+
+    /*-- Callback function for subscriber of the mocap system --*/
+
+    mocapPosition[0]=mocapmsg.pose.position.x;
+    mocapPosition[1]=mocapmsg.pose.position.y;
+    mocapPosition[2]=mocapmsg.pose.position.z;
+
+
+    mocapTime.push_back((ros::Time::now().toSec())-startTime);
+
+    for(int i=0;i<3;i++){
+
+        mocapHistoryPosition[i].push_back(mocapPosition[i]);
+
+    }
+
+    if(mocapCounter>0){
+       std::vector<double> previousSample(3,0);
+       for(int i=0;i<3;i++){
+           previousSample[i]=mocapHistoryPosition[i][mocapCounter-1];
+       }
+       mocapVelocity=calcDtVelocity(mocapPosition,previousSample,(double)1/std::min((double)sRate,(double)mocapRate));
+
+        for(int i=0;i<3;i++){
+            mocapHistoryVelocity[i].push_back(mocapVelocity[i]);
+        }
+        velocityNormHistory.push_back(velocityNorm(mocapHistoryVelocity,(int)(lookBack*sRate)));
+        //checkVelocityHistory.push_back(check_velocity(velocityNormHistory.back(),velThreshold));
+        //checkVelocityHistory.push_back(1);
+       // std::cout<<"velocity: " << check_velocity(velocityNormHistory.back(),velThreshold) << "\n";// << velocityNormHistory.back() << " "
+
+
+
+    }
+
+    //std::cout<<"mocap listener okokkokoko\n";
+
+
+    mocapCounter++;
+    //ROS_INFO("I heard: [%d] messages from mocap\n", mocapCounter);
+}
+
+
+void rightHandListener(const sensor_msgs::JointState& jointStatemsg){
+
+    /*-- Callback function for subscriber of the right Allegro hand --*/
+
+    for(int i=0;i<16;i++){
+
+         rightHandHistory[i].push_back(jointStatemsg.position[i]);
+    }
+
+
+    if(allegroCounter<1) {
+
+        rightHandHistory[16].push_back((ros::Time::now().toSec())-startTime);
+
+        //initialTime=tm2double(jointStatemsg.header.stamp.sec,jointStatemsg.header.stamp.nsec);
+
+    }else{
+        //double tmp=tm2double(jointStatemsg.header.stamp.sec,jointStatemsg.header.stamp.nsec)-initialTime;
+
+        //rightHandHistory[16].push_back(tmp);
+        rightHandHistory[16].push_back((ros::Time::now().toSec())-startTime);
+
+
+    }
+
+    allegroCounter++;
+
+}
+
+
+void leftHandListener(const sensor_msgs::JointState& jointStatemsg){
+
+    /*-- Callback function for subscriber of the left Allegro hand --*/
+
+    for(int i=0;i<16;i++){
+
+         leftHandHistory[i].push_back(jointStatemsg.position[i]);
+    }
+
+
+    if(allegroCounter<1) {
+
+        leftHandHistory[16].push_back((ros::Time::now().toSec())-startTime);
+
+        //initialTime=tm2double(jointStatemsg.header.stamp.sec,jointStatemsg.header.stamp.nsec);
+
+    }else{
+        //double tmp=tm2double(jointStatemsg.header.stamp.sec,jointStatemsg.header.stamp.nsec)-initialTime;
+
+        //leftHandHistory[16].push_back(tmp);
+        leftHandHistory[16].push_back((ros::Time::now().toSec())-startTime);
+
+    }
+
+    allegroCounter++;
+
+}
+
+
+
+int main(int argc, char **argv)
+{
+
+
+    // set the configurations of the hands
+    std::vector< std::vector<double> > rightHandConfiguration=handConfiguations(right, nbClasses);
+
+    std::vector< std::vector<double> > leftHandConfiguration=handConfiguations(left, nbClasses);
+
+    // set the velocity of the joints to zero (maximum)
+    std::vector<double> joint_velocity(16,0);
+
+
+    // set the messages that will include the desired joint angles to the allegro node. This message will contain the velocity and the position of the joints
+    sensor_msgs::JointState righHand_msg;
+
+    sensor_msgs::JointState leftHand_msg;
+
+    // set the message for publishing the graps type
+    decision_maker::vtmsg graspmsg;
+
+
+    // resize the vectors of position and velocity according to the numbers of DOF of the allegro hand
+    righHand_msg.velocity.resize(16);
+    righHand_msg.position.resize(16);
+    leftHand_msg.velocity.resize(16);
+    leftHand_msg.position.resize(16);
+
+
+    // setting the velocity of the joints to maximum
+    righHand_msg.velocity=joint_velocity;
+    leftHand_msg.velocity=joint_velocity;
+
+
+    // initialize the node
+    ros::init(argc, argv, "decisionMaker");
+
+    ros::NodeHandle n;
+
+
+    // set the publishers for the allegro hand
+
+    ros::Publisher allegorRight_pub = n.advertise<sensor_msgs::JointState>("allegroHand_1/joint_cmd", 100);
+
+    ros::Publisher allegorLeft_pub = n.advertise<sensor_msgs::JointState>("allegroHand_0/joint_cmd", 100);
+
+
+
+    // set a publisher for publishing the grasp type
+
+    ros::Publisher graspType_pub=n.advertise<decision_maker::vtmsg>("decisionMaker/grasp_type", 100);
+
+
+
+    // set the subscribers to listen the classification outcome from the windows machine and the position of the hand
+
+    ros::Subscriber daqSub = n.subscribe("win_pub", 2, daqListener);
+
+    ros::Subscriber mocapSub=n.subscribe("HAND/pose", 10, mocapListener);
+
+
+
+    // set subscribers to listen the joint states of the Allegro hands
+
+    ros::Subscriber allegorRight_sub = n.subscribe("allegroHand_1/joint_states", 1000, rightHandListener);
+
+    ros::Subscriber allegorLeft_sub=n.subscribe("allegroHand_0/joint_states", 1000, leftHandListener);
+
+
+
+    //startTime=ros::Time::now().toSec();
+
+    // set the loop rate
+    ros::Rate loop_rate(sRate);
+
+
+
+
+    int count = 0;
+    while (ros::ok())
+    {
+        // set the joint position of the allegro hand
+
+        righHand_msg.position=rightHandConfiguration[grasp_type];
+        leftHand_msg.position=leftHandConfiguration[grasp_type];
+
+        // set the grasp type
+
+        graspmsg.vote=grasp_type;
+
+        // publish the messages
+
+        allegorRight_pub.publish(righHand_msg);
+        allegorLeft_pub.publish(leftHand_msg);
+        graspType_pub.publish(graspmsg);
+
+        // if the key 't' is pressed, save the data and clear the data for the next trial
+
+        if(getch_()=='t'){
+            saveRecordings();
+
+        }
+
+
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+        ++count;
+    }
+
+
+    return 0;
+}
+
+
+void saveRecordings(){
+
+    /*
+     * This function saves the data of each trial to mat files and clears the vector
+     *
+     *
+     */
+
+
+
+    // create directory inside the folder data for saving the data of the trial
+
+    std::string dirName="src/decision_maker/data/trial"+std::to_string(trialCounter+1);
+
+    if (stat(dirName.c_str(), &st) == -1) {
+        mkdir(dirName.c_str(), 0700);
+    }
+
+
+    // save the data to the folder
+
+    if(!mocapHistoryPosition[0].empty()) saveData("src/decision_maker/data/trial"+std::to_string(trialCounter+1)+"/mocapHistoryPosition","mocapHistoryPosition",mocapHistoryPosition);
+    if(!mocapHistoryVelocity[0].empty()) saveData("src/decision_maker/data/trial"+std::to_string(trialCounter+1)+"/mocapHistoryVelocity","mocapHistoryVelocity",mocapHistoryVelocity);
+
+    if(!mocapTime.empty()) writeMatFile("src/decision_maker/data/trial"+std::to_string(trialCounter+1)+"/mocapTime","mocapTime",mocapTime);
+    //if(!checkVelocityHistory.empty()) writeMatFile("src/decision_maker/data/trial"+std::to_string(trialCounter+1)+"/checkVelocityHistory","checkVelocityHistory",checkVelocityHistory);
+    if(!graspTypeHistory.empty()) writeMatFile("src/decision_maker/data/trial"+std::to_string(trialCounter+1)+"/graspTypeHistory","graspTypeHistory",graspTypeHistory);
+    if(!velocityNormHistory.empty()) writeMatFile("src/decision_maker/data/trial"+std::to_string(trialCounter+1)+"/velocityNormHistory","velocityNormHistory",velocityNormHistory);
+    if(!graspTime.empty()) writeMatFile("src/decision_maker/data/trial"+std::to_string(trialCounter+1)+"/graspTime","graspTime",graspTime);
+
+    if(!rightHandHistory[0].empty()) saveData("src/decision_maker/data/trial"+std::to_string(trialCounter+1)+"/rightHandHistory","rightHandHistory",rightHandHistory);
+    if(!leftHandHistory[0].empty()) saveData("src/decision_maker/data/trial"+std::to_string(trialCounter+1)+"/leftHandHistory","leftHandHistory",leftHandHistory);
+
+
+    std::cout<<"\nTrial " << trialCounter+1 <<" has been saved\n";
+
+    // clear the data for the next trial
+
+    for(int i=0;i<(int)mocapHistoryPosition.size();i++){
+        mocapHistoryPosition[i].clear();
+    }
+
+    for(int i=0;i<(int)mocapHistoryVelocity.size();i++){
+        mocapHistoryVelocity[i].clear();
+    }
+
+    mocapTime.clear();
+    checkVelocityHistory.clear();
+    graspTypeHistory.clear();
+    mVotes.clear();
+    velocityNormHistory.clear();
+
+    if(!rightHandHistory[0].empty()){
+        for(int i=0;i<(int)rightHandHistory.size();i++){
+            rightHandHistory[i].clear();
+        }
+    }
+
+    if(!leftHandHistory[0].empty()){
+        for(int i=0;i<(int)leftHandHistory.size();i++){
+            leftHandHistory[i].clear();
+        }
+    }
+
+    std::cout<<"Data cleared\n";
+    std::cout<<"Ready for the next trial\n";
+    trialCounter++;
+    grasp_type=0;
+
+}
+
+int getch_(){
+  static struct termios oldt, newt;
+  tcgetattr( STDIN_FILENO, &oldt);                  // save old settings
+
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON);                        // disable buffering
+  newt.c_cc[VMIN] = 0; newt.c_cc[VTIME] = 0;
+  tcsetattr( STDIN_FILENO, TCSANOW, &newt);         // apply new settings
+
+  int c = getchar();                                // read character (non-blocking)
+
+  tcsetattr( STDIN_FILENO, TCSANOW, &oldt);         // restore old settings
+  return c;
+}
+
+
